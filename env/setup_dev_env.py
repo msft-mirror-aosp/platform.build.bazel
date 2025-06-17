01049904c5980a33532ae1c07601542e4b115bf0
Ruiqi Mao <ruiqimao@google.com>
2025-06-17 20:04:08 +0000

    Create setup_dev_env.py script
    The script will be run in Python (not through Bazel) to perform initial
    local dev setup.
    
    Bug: 424205205
    Test: setup_dev_env_test
    Change-Id: If7515dcaf6e53d32411da2f23a620632f5a6561c

---
+++ env/setup_dev_env.py
@@ -0,0 +1,77 @@
+"""Script to set up a local development environment."""
+
+import enum
+import logging
+import pathlib
+import platform
+
+
+class Platform(enum.Enum):
+  UNKNOWN        = 0
+  GLINUX_DESKTOP = 1
+  GLINUX_LAPTOP  = 2
+  WINDOWS        = 3
+  MAC            = 4
+
+
+def detect_platform() -> Platform:
+  """Returns the platform the script is running on."""
+  system = platform.system()
+  if system == 'Linux':
+    lsb_release = pathlib.Path('/etc/lsb-release').read_text()
+    if 'GOOGLE_ROLE=desktop' in lsb_release:
+      return Platform.GLINUX_DESKTOP
+    if 'GOOGLE_ROLE=laptop' in lsb_release:
+      return Platform.GLINUX_LAPTOP
+  if system == 'Windows':
+    return Platform.WINDOWS
+  if system == 'Darwin':
+    return Platform.MAC
+  return Platform.UNKNOWN
+
+
+def find_workspace_root() -> pathlib.Path:
+  """Returns the path to the workspace root.
+
+  Raises:
+    FileNotFoundError: If the workspace root could not be found.
+  """
+  path = pathlib.Path(__file__)
+  while True:
+    repo = path / '.repo'
+    if repo.exists() and repo.is_dir():
+      return path
+    if path == path.parent:
+      raise FileNotFoundError('Could not find workspace root')
+    path = path.parent
+
+
+def build_env_bazelrc(platform: Platform) -> str:
+  """Returns the env.bazelrc file for the given platform."""
+  bazelrc = ''
+  if platform is Platform.GLINUX_DESKTOP:
+    bazelrc += 'import %workspace%/tools/vendor/google/bazel/glinux.bazelrc\n'
+  else:
+    bazelrc += 'common --google_default_credentials\n'
+  bazelrc += 'common --config=rcache\n'
+  bazelrc += 'common --config=release\n'
+  return bazelrc
+
+
+def main():
+  logging.basicConfig(level=logging.INFO)
+
+  platform = detect_platform()
+  workspace_root = find_workspace_root()
+
+  logging.info('Detected platform %s', platform)
+  logging.info('Found workspace root %s', workspace_root)
+
+  env_bazelrc_contents = build_env_bazelrc(platform)
+  env_bazelrc_path = workspace_root / 'env.bazelrc'
+  env_bazelrc_path.write_text(env_bazelrc_contents)
+  logging.info('Wrote env.bazelrc')
+
+
+if __name__ == '__main__':
+  main()
